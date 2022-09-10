8140fcde98a7dca50fbe1563792468712b78869d
rizemon <tan.jia.le.98@gmail.com>
2022-09-11 03:23:15 +0800

    Add env vars to .bashrc

--- setup_cicd_user.sh
+++ setup_cicd_user.sh
@@ -12,5 +12,9 @@ loginctl enable-linger cicd
 
 runuser -l cicd -c 'curl -fsSL https://get.docker.com/rootless -o ~/get-docker.sh'
 runuser -l cicd -c 'chmod +x ~/get-docker.sh && XDG_RUNTIME_DIR=/run/user/$(id -u) ~/get-docker.sh'
-runuser -l cicd -c 'echo "export PATH=/home/cicd/bin:\$PATH" >> ~/.bash_profile'
-runuser -l cicd -c 'echo "export DOCKER_HOST=unix:///run/user/$(id -u)/docker.sock" >> ~/.bash_profile'
+
+NEW_PATH="$(runuser -l cicd -c 'echo $PATH')"
+NEW_DOCKER_HOST="unix:///run/user/$(id -u cicd)/docker.sock"
+
+runuser -l cicd -c "echo 'export PATH=${NEW_PATH}' | tee -a ~/.bash_profile ~/.bashrc"
+runuser -l cicd -c "echo 'export DOCKER_HOST=${NEW_DOCKER_HOST}' | tee -a ~/.bash_profile ~/.bashrc"
